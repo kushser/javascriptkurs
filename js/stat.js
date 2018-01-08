6da177522c178d04fd372d4a20cf501ed41abf38
kushser <kush_ser@ukr.net>
2018-01-08 17:09:38 +0200

    добавил текст

--- js/stat.js
+++ js/stat.js
@@ -1,4 +1,9 @@
 window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = 'red';
   ctx.fillRect(100, 10, 420, 270);
+
+  ctx.fillStyle = '#000';
+  ctx.font = '14px PT Mono';
+  ctx.fillText('Ура Вы победили', 120, 40);
+
 };
